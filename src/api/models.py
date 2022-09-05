151f9ad44fda2b0d0dfdd2aabe0348a03abb506b
Phherten <phigueraherten@gmail.com>
2022-09-05 18:26:54 +0000

    Comienzo del proyecto, bases de datos MafiaMembers creada

--- src/api/models.py
+++ src/api/models.py
@@ -17,3 +17,53 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+    def save(self):
+        if not self.id:
+            db.session.add(self)
+        db.session.commit()
+
+
+class MafiaMember(db.Model):
+    __tablename__ = "mafia_member"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    boss = db.Column(db.String(120), unique=False, nullable=False)
+    seniority = db.Column(db.Integer, unique=False, nullable=False)
+    is_free = db.Column(db.Boolean(), unique=False, nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "boss":self.boss,
+            "seniority":self.seniority,
+            "Is free":self.is_free
+            # do not serialize the password, its a security breach
+        }
+    def save(self):
+        if not self.id:
+            db.session.add(self)
+        db.session.commit()
+    
+
+class Subordinate(db.Model):
+    __tablename__ = "subordinate"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=False, nullable=False)
+    subordinate = db.Column(db.String(120), unique=True, nullable=False)
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "subordinate":self.subordinate  
+        }
+    
+    def save(self):
+        if not self.id:
+            db.session.add(self)
+        db.session.commit()
+
+    
+
+
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,8 +2,10 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, MafiaMember, Subordinate
 from api.utils import generate_sitemap, APIException
+import requests
+import json
 
 api = Blueprint('api', __name__)
 
@@ -14,5 +16,69 @@ def handle_hello():
     response_body = {
         "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
     }
-
     return jsonify(response_body), 200
+
+
+@api.route('/add', methods=['POST', 'GET'])
+def add():
+    url = "https://run.mocky.io/v3/6d754a01-9529-47fe-a6ee-8037836b8333"
+    payload = {}
+    headers = {}
+    response = requests.request("GET", url, headers=headers, data=payload)
+    response_json = json.loads(response.text)
+    members = response_json["members"]
+    
+    for i in members:
+        member = i
+        mafiaMember = MafiaMember(
+            name = member["name"],
+            boss = member["boss"],
+            seniority = member["seniority"],
+            is_free = True
+        )
+        mafiaMember.save()
+    return "todo Ok"
+
+@api.route('/add_subordinate', methods=['POST', 'GET'])
+def add_subordinate():
+    url = "https://run.mocky.io/v3/6d754a01-9529-47fe-a6ee-8037836b8333"
+    payload = {}
+    headers = {}
+    response = requests.request("GET", url, headers=headers, data=payload)
+    response_json = json.loads(response.text)
+    members = response_json["members"]
+    
+    for i in members:
+        member = i
+        if member["subordinates"]:
+            member_subordinates = member["subordinates"]
+            for i in member_subordinates:
+                subordinate = i
+                subordinates = Subordinate(
+                    name = member["name"],
+                    subordinate = subordinate
+                )
+                subordinates.save()
+                    
+        
+    return "todo Ok"
+
+
+    
+         
+
+            
+        
+
+        
+        
+        
+    
+
+
+
+    
+
+    
+
+
